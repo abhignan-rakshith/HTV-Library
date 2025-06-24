d5413b2ae7b4cc2a14189d931b078f4400a296c2
Abhignan Rakshith <abhignanrakshith@icloud.com>
2025-06-25 00:34:06 +0530

    feat: Enhance HTV Library Application
    - Updated application description in package.json for clarity.
    - Added dependencies for adblocking and fetch functionality.
    - Improved CSS for a better layout and full-screen experience.
    - Modified index.html to load Hanime.tv in a webview.
    - Refactored index.js to initialize adblocker and set window properties.
    - Implemented context bridge in preload.js for secure IPC communication.

--- forge.config.js
+++ forge.config.js
@@ -3,7 +3,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
   packagerConfig: {
-    asar: true,
+    asar: true
   },
   rebuildConfig: {},
   makers: [
@@ -29,8 +29,6 @@ module.exports = {
       name: '@electron-forge/plugin-auto-unpack-natives',
       config: {},
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
